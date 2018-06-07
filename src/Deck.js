c71e9be5a1cdbe2ef8c1331aaec4abc7a8e345b5
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-06-08 00:27:36 +0700

    Render some deck on screen

---
+++ src/Deck.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Animated, PanResponder } from 'react-native';
+
+class Deck extends Component {
+    constructor(props) {
+        super(props);
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: () => { },
+            onPanResponderRelease: () => { }
+        });
+
+        this.state = {
+            panResponder
+        }
+    }
+    renderCards = () => {
+        return this.props.data.map((item) => {
+            return this.props.renderCard(item);
+        });
+    }
+    render() {
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        );
+    }
+}
+
+export default Deck;
