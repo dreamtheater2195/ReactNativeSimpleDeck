14b39dd6cc375b0b267421c43d016610f0363eb8
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-06-08 14:23:42 +0700

    Rotate the first card left or right

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Animated, PanResponder } from 'react-native';
+import { View, StyleSheet, Animated, PanResponder, Dimensions } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25;
+const SWIPE_OUT_DURATION = 250;
 class Deck extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +15,15 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => { }
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
 
         this.state = {
@@ -20,18 +31,58 @@ class Deck extends Component {
             position
         };
     }
+
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: { x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start();
+    }
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
+        }).start((() => this.onSwipeComplete(direction)));
+    }
+
+    onSwipeComplete(direction) {
+        const { onSwipeLeft, onSwipeRight } = this.props;
+        direction === 'right' ? onSwipeRight() : onSwipeLeft();
+    }
+
+    getCardStyle() {
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        }
+    }
     renderCards = () => {
-        return this.props.data.map((item) => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
             return this.props.renderCard(item);
         });
     }
     render() {
         return (
-            <Animated.View style={this.state.position.getLayout()}>
-                <View {...this.state.panResponder.panHandlers}>
-                    {this.renderCards()}
-                </View>
-            </Animated.View>
+            <View>
+                {this.renderCards()}
+            </View>
         );
     }
 }
